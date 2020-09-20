0769b67e1a935373ef1fe0ea8840fe39a98021b8
vrcrush <vrcrush@hotmail.com>
2020-09-20 07:13:16 -0500

    first commit

---
+++ Myfirstproject/src/Myfirstproject.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Myfirstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter ypir name");
+Scanner in = new Scanner(System.in);
+
+int i = 0;
+do {
+	System.out.println("I is: "+i);
+	i++;
+	
+}while (i <10);
+	
+in.close();
+
+	}
+
+}
