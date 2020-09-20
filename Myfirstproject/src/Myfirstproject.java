4a50769c022a66832baef562be46f278e9255341
vrcrush <vrcrush@hotmail.com>
2020-09-20 16:26:22 -0500

    reading file

--- Myfirstproject/src/Myfirstproject.java
+++ Myfirstproject/src/Myfirstproject.java
@@ -1,19 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Myfirstproject {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-System.out.println("Enter ypir name");
-Scanner in = new Scanner(System.in);
+//System.out.println("Enter ypir name"); 
+		
+Scanner in = new Scanner(new File("students.txt"));
+ArrayList<String> students =new ArrayList<String>();
 
-int i = 0;
-do {
-	System.out.println("I is: "+i);
-	i++;
-	
-}while (i <10);
-	
+while(in.hasNextLine()) {
+	students.add(in.nextLine());
+	}
+
+for (int i = 0;i < students.size(); i++) {
+	System.out.println("name: "+ students.get(i));
+}
 in.close();
 
 	}
